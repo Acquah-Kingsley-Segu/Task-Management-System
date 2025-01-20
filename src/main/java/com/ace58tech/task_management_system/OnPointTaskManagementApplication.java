d5397c785a4fd5a26b0be6abf52de5516e42690b
KingsleySeguAcquah <acquahkingsleysegu10@gmail.com>
2025-01-20 22:22:28 +0000

    Project setup for the task management system

---
+++ src/main/java/com/ace58tech/task_management_system/OnPointTaskManagementApplication.java
@@ -0,0 +1,13 @@
+package com.ace58tech.task_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnPointTaskManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnPointTaskManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ace58tech/task_management_system/OnPointTaskManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ace58tech.task_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnPointTaskManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
