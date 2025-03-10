977f558bebf3edb98c59dc7d5e731bb577acea42
Acquah-Kingsley-Segu <acquahkingsleysegu10@gmail.com>
2025-03-10 23:29:37 +0000

    Creates all the individual modules of the application

---
+++ apps/organization-management-service/src/test/java/com/ace58tech/organization_management_service/OrganizationManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ace58tech.organization_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizationManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ apps/task-management-gateway/src/main/java/com/ace58tech/task_management_gateway/TaskManagementGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.ace58tech.task_management_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagementGatewayApplication.class, args);
+	}
+
+}
